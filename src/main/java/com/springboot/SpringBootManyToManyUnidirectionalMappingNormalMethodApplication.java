611f926ded6ad736f2b45a1568c4e7f5006fe291
HP <HP@DESKTOP-CPLQTK2>
2023-10-09 20:29:33 +0530

    SpringBoot JPA ManyToMany Unidirectional Mapping

---
+++ src/main/java/com/springboot/SpringBootManyToManyUnidirectionalMappingNormalMethodApplication.java
@@ -0,0 +1,13 @@
+package com.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootManyToManyUnidirectionalMappingNormalMethodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootManyToManyUnidirectionalMappingNormalMethodApplication.class, args);
+	}
+
+}
